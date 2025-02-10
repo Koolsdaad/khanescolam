e6372115f06a399a2e03faec0cb8bb91bcdc6560
Koolsdaad <t99962018@gmail.com>
2025-02-10 17:23:41 -0300

    Update widgetBot.js

--- visuals/widgetBot.js
+++ visuals/widgetBot.js
@@ -3,7 +3,7 @@ if(!device.mobile) {
         src: 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3',
         async: true,
         onload: () => {
-            const discEmbed = new Crate({ server: '1286573512831533056', channel: '1286573601687867433',
+            const discEmbed = new Crate({ server: '1338605927116574730', channel: '1338605927972077663',
                 location: ['bottom', 'right'], notifications: true, indicator: true, allChannelNotifications: true,
                 defer: false, color: '#000000'
             });
