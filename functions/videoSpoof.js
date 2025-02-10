6255ae52bcf0db5357fd8a934230824c7f0dd3c3
Koolsdaad <t99962018@gmail.com>
2025-02-10 18:42:40 -0300

    Update videoSpoof.js

--- functions/videoSpoof.js
+++ functions/videoSpoof.js
@@ -14,7 +14,7 @@ window.fetch = async function (input, init) {
                 body = JSON.stringify(bodyObj);
                 if (input instanceof Request) { input = new Request(input, { body: body }); } 
                 else init.body = body; 
-                sendToast("video hakiado", 1000)
+                sendToast("Video Pulado", 1000)
             }
         } catch (e) { debug(`🚨 Error @ videoSpoof.js\n${e}`); }
     }
