13537d2477019716c9d4e65c10bd7167a31fed96
Koolsdaad <t99962018@gmail.com>
2025-02-10 18:36:52 -0300

    Update answerRevealer.js

--- functions/answerRevealer.js
+++ functions/answerRevealer.js
@@ -15,7 +15,7 @@ JSON.parse = function (e, t) {
                                 widget.options.choices.forEach(choice => {
                                     if (choice.correct) {
                                         choice.content = "✅ " + choice.content;
-                                        sendToast("🔓 Respostas reveladas.", 1000);                
+                                        sendToast("Respostas reveladas.", 1000);                
                                     }
                                 });
                             }
